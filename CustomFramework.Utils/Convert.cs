04bf20037f3861b66c3c0bdc18573f43b050cb65
Yunus Emre <ykirkanahtar@hotmail.com>
2018-08-15 13:29:35 +0300

    Kart formatlama metodu eklendi

--- CustomFramework.Utils/Convert.cs
+++ CustomFramework.Utils/Convert.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
 
 namespace CustomFramework.Utils
 {
@@ -11,6 +12,17 @@ namespace CustomFramework.Utils
             var startUnderscores = Regex.Match(input, @"^_+");
             return startUnderscores + Regex.Replace(input, @"([a-z0-9])([A-Z])", "$1_$2")
                        .ToLower().Replace('ı', 'i');
+        }
+
+        public static string FormatCardNumber(string cardNumber)
+        {
+            var firstDigits = cardNumber.Substring(0, 6);
+            var lastDigits = cardNumber.Substring(cardNumber.Length - 4, 4);
+
+            var requiredMask = new String('X', cardNumber.Length - firstDigits.Length - lastDigits.Length);
+
+            var maskedString = string.Concat(firstDigits, requiredMask, lastDigits);
+            return Regex.Replace(maskedString, ".{4}", "$0 ");
         }
 
     }
