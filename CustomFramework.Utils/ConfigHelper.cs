9f7f4a82f87384249ea83b0e55d6dfcad22a4b23
Yunus Emre <ykirkanahtar@hotmail.com>
2019-01-11 00:18:03 +0300

    ConfigHelper sınıfı eklendi

---
+++ CustomFramework.Utils/ConfigHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace CustomFramework.Utils
+{
+    public static class ConfigHelper
+    {
+        private static IConfigurationRoot Configuration { get; set; }
+
+        public static string GetConfigurationValue(string key, string fileName = "appsettings.json")
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(fileName);
+            Configuration = builder.Build();
+
+            return Configuration[key];
+        }
+    }
+}
