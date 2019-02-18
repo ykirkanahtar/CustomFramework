6c1b99c1b621f0a89157a4c3d6bc1ce0dbbaf053
Yunus Emre <yunusemre.kirkanahtar@cutesafe.com>
2019-02-18 23:24:25 +0300

    Login sınıfında kullanıcı adı, e-posta alanı ile değiştirildi

--- CustomFramework.WebApiUtils.Authorization.Contracts/Requests/Login.cs
+++ CustomFramework.WebApiUtils.Authorization.Contracts/Requests/Login.cs
@@ -3,7 +3,7 @@
     public class Login
     {
         public int ApplicationId { get; set; }
-        public string UserName { get; set; }
+        public string Email { get; set; }
         public string UserPassword { get; set; }
         public string ClientApplicationCode { get; set; }
         public string ClientApplicationPassword { get; set; }
