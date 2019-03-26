0ad4c6f6b4aa4d9843642d15e4490aa354760901
Yunus Emre <yunusemre.kirkanahtar@cutesafe.com>
2019-03-26 16:57:59 +0300

    Kullanıcıları rollere atayan sınıflar geliştirildi

--- CustomFramework.WebApiUtils.Identity.Contracts/Requests/UserAddToRolesRequest.cs
+++ CustomFramework.WebApiUtils.Identity.Contracts/Requests/UserAddToRolesRequest.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace CustomFramework.WebApiUtils.Identity.Contracts.Requests
 {
-    public class UserAddToRoleRequest
+    public class UserAddToRolesRequest
     {
         public int UserId { get; set; }
         public List<string> Roles { get; set; }
---
+++ CustomFramework.WebApiUtils.Identity.Contracts/Requests/UserRemoveFromRolesRequest.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace CustomFramework.WebApiUtils.Identity.Contracts.Requests
+{
+    public class UserRemoveFromRolesRequest : UserAddToRolesRequest
+    {
+        
+    }
+}
