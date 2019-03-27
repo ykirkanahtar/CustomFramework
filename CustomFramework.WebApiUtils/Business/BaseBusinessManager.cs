c8cef4475dbeed215f25f9dc6fef34d114bfaa4e
Yunus Emre <yunusemre.kirkanahtar@cutesafe.com>
2019-03-27 15:49:18 +0300

    Identity desteği eklendi

--- CustomFramework.WebApiUtils/Business/BaseBusinessManager.cs
+++ CustomFramework.WebApiUtils/Business/BaseBusinessManager.cs
@@ -2,8 +2,10 @@
 using CustomFramework.WebApiUtils.Constants;
 using CustomFramework.WebApiUtils.Enums;
 using CustomFramework.WebApiUtils.Utils;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace CustomFramework.WebApiUtils.Business
@@ -12,11 +14,19 @@ namespace CustomFramework.WebApiUtils.Business
     {
         private readonly ILogger<BaseBusinessManager> _logger;
         protected readonly IMapper Mapper;
+        protected readonly int UserId;
 
         protected BaseBusinessManager(ILogger<BaseBusinessManager> logger, IMapper mapper)
         {
             _logger = logger;
             Mapper = mapper;
+        }
+
+        protected BaseBusinessManager(ILogger<BaseBusinessManager> logger, IMapper mapper, IHttpContextAccessor httpContextAccessor)
+        {
+            _logger = logger;
+            Mapper = mapper;
+            UserId = Convert.ToInt32(httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
         }
 
         protected async Task<T> CommonOperationAsync<T>(Func<Task<T>> func, BusinessBaseRequest businessBaseRequest)
