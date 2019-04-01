b7c41c240ca83ebb25d333e5cd9dd3236c1b8436
Yunus Emre <yunusemre.kirkanahtar@cutesafe.com>
2019-04-01 18:02:43 +0300

    LocalizedClaimResponse sınıfı eklendi

---
+++ CustomFramework.WebApiUtils.Identity.Contracts/Responses/LocalizedClaimResponse.cs
@@ -0,0 +1,8 @@
+namespace CustomFramework.WebApiUtils.Identity.Contracts.Responses
+{
+    public class LocalizedClaimResponse
+    {
+        public string ClaimType { get; set; }
+        public string LocalizedClaimType { get; set; }
+    }
+}
