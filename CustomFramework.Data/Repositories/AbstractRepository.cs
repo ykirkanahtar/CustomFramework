f0babe2ac39bf6d12ad6a3987d419def201bc86a
Yunus Emre <yunusemre.kirkanahtar@cutesafe.com>
2019-04-12 09:50:03 +0300

    Get metotlarına pasifler de eklendi

--- CustomFramework.Data/Repositories/AbstractRepository.cs
+++ CustomFramework.Data/Repositories/AbstractRepository.cs
@@ -1,189 +1,185 @@
-﻿using CustomFramework.Data.Contracts;
-using CustomFramework.Data.Enums;
-using CustomFramework.Data.Models;
-using LinqKit;
-using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
+using CustomFramework.Data.Contracts;
+using CustomFramework.Data.Enums;
+using CustomFramework.Data.Models;
 using CustomFramework.Data.Utils;
+using LinqKit;
+using Microsoft.EntityFrameworkCore;
 
 namespace CustomFramework.Data.Repositories
 {
     public abstract class AbstractRepository<TEntity, TKey> : IBaseRepository<TEntity, TKey>
-           where TEntity : BaseModelNonUser<TKey>
-    {
-        protected readonly DbContext DbContext;
-        protected readonly DbSet<TEntity> DbSet;
-        private bool _disposed;
-        private readonly Expression<Func<TEntity, object>>[] _includeProperties = { };
-
-        protected AbstractRepository(DbContext dbContext)
+        where TEntity : BaseModelNonUser<TKey>
         {
-            DbContext = dbContext;
-            DbSet = DbContext.Set<TEntity>();
-        }
+            protected readonly DbContext DbContext;
+            protected readonly DbSet<TEntity> DbSet;
+            private bool _disposed;
+            private readonly Expression<Func<TEntity, object>>[] _includeProperties = { };
 
-        protected AbstractRepository(DbContext dbContext, Expression<Func<TEntity, object>>[] includeProperties)
-        {
-            DbContext = dbContext;
-            DbSet = DbContext.Set<TEntity>();
-            _includeProperties = includeProperties;
-        }
+            protected AbstractRepository(DbContext dbContext)
+            {
+                DbContext = dbContext;
+                DbSet = DbContext.Set<TEntity>();
+            }
 
-        protected AbstractRepository(DbContext dbContext, bool eager)
-        {
-            DbContext = dbContext;
-            DbSet = DbContext.Set<TEntity>();
-        }
+            protected AbstractRepository(DbContext dbContext, Expression<Func<TEntity, object>>[] includeProperties)
+            {
+                DbContext = dbContext;
+                DbSet = DbContext.Set<TEntity>();
+                _includeProperties = includeProperties;
+            }
 
-        #region IRepository members
+            protected AbstractRepository(DbContext dbContext, bool eager)
+            {
+                DbContext = dbContext;
+                DbSet = DbContext.Set<TEntity>();
+            }
 
-        public async virtual Task<TEntity> GetByIdAsync(TKey id)
-        {
-            var query = from p in DbContext.Set<TEntity>()
-                        where p.Id.Equals(id) && p.Status == Status.Active
-                        select p;
+            #region IRepository members
 
-            //var s = query.ToSql();
-
-            query = _includeProperties.Aggregate(query, (current, includedProperty) => current.Include(includedProperty));
-
-            return await query.FirstOrDefaultAsync();
-        }
-
-        public virtual TEntity GetById(TKey id)
-        {
-            var query = from p in DbContext.Set<TEntity>()
+            public async virtual Task<TEntity> GetByIdAsync(TKey id)
+            {
+                var query = from p in DbContext.Set<TEntity>()
                 where p.Id.Equals(id) && p.Status == Status.Active
                 select p;
 
-            query = _includeProperties.Aggregate(query, (current, includedProperty) => current.Include(includedProperty));
+                //var s = query.ToSql();
 
-            return query.FirstOrDefault();
-        }
+                query = _includeProperties.Aggregate(query, (current, includedProperty) => current.Include(includedProperty));
 
-        public virtual IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> predicate)
-        {
-            return DbSet.Where(PredicateBuild(predicate));
-        }
-
-        //OrderBy kullanımı örneği : GetAll(orderBy: q => q.OrderByDescending(s => s.CreateDateTime), take: 10)
-        public virtual IQueryable<TEntity> GetAll(
-            Expression<Func<TEntity, bool>> predicate = null
-            , Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null
-            , int? take = null
-        )
-        {
-            IQueryable<TEntity> query = DbSet;
-
-            query = query.Where(predicate != null ? PredicateBuild(predicate) : PredicateBuild());
-            query = _includeProperties.Aggregate(query, (current, includedProperty) => current.Include(includedProperty));
-
-            if (orderBy != null)
-            {
-                query = orderBy(query);
+                return await query.FirstOrDefaultAsync();
             }
 
-            if (take != null)
+            public virtual TEntity GetById(TKey id)
             {
-                query = query.Take((int)take);
+                var query = from p in DbContext.Set<TEntity>()
+                where p.Id.Equals(id) && p.Status == Status.Active
+                select p;
+
+                query = _includeProperties.Aggregate(query, (current, includedProperty) => current.Include(includedProperty));
+
+                return query.FirstOrDefault();
             }
 
-            return query;
-        }
-
-        public virtual async Task<ICustomQueryable<TEntity>> GetAllWithPagingAsync(
-            IPaging paging
-            , Expression<Func<TEntity, bool>> predicate = null
-            , Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null
-        )
-        {
-            IQueryable<TEntity> query = DbSet;
-
-            query = query.Where(predicate != null ? PredicateBuild(predicate) : PredicateBuild());
-
-            var rowCount = await query.CountAsync();
-            query = _includeProperties.Aggregate(query, (current, includedProperty) => current.Include(includedProperty));
-
-
-            if (orderBy != null)
+            public virtual IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> predicate, bool selectPassives = false)
             {
-                query = orderBy(query);
+                return DbSet.Where(PredicateBuild(predicate, selectPassives));
             }
 
-            query = query.Skip(Math.Abs(paging.PageIndex - 1) * paging.PageSize).Take(paging.PageSize);
-
-            return new CustomQueryable<TEntity>
+            //OrderBy kullanımı örneği : GetAll(orderBy: q => q.OrderByDescending(s => s.CreateDateTime), take: 10)
+            public virtual IQueryable<TEntity> GetAll(
+                Expression<Func<TEntity, bool>> predicate = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, int? take = null, bool selectPassives = false
+            )
             {
-                Result = query,
-                Count = rowCount
-            };
-        }
+                IQueryable<TEntity> query = DbSet;
 
-        public virtual ICustomQueryable<TEntity> GetAllWithPaging(
-            IPaging paging
-            , Expression<Func<TEntity, bool>> predicate = null
-            , Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null
-        )
-        {
-            IQueryable<TEntity> query = DbSet;
+                query = query.Where(predicate != null ? PredicateBuild(predicate, selectPassives) : PredicateBuild(selectPassives));
+                query = _includeProperties.Aggregate(query, (current, includedProperty) => current.Include(includedProperty));
 
-            query = query.Where(predicate != null ? PredicateBuild(predicate) : PredicateBuild());
+                if (orderBy != null)
+                {
+                    query = orderBy(query);
+                }
 
-            var rowCount = query.Count();
-            query = _includeProperties.Aggregate(query, (current, includedProperty) => current.Include(includedProperty));
+                if (take != null)
+                {
+                    query = query.Take((int) take);
+                }
 
-            if (orderBy != null)
-            {
-                query = orderBy(query);
+                return query;
             }
 
-            query = query.Skip((paging.PageIndex - 1) * paging.PageSize).Take(paging.PageSize);
-
-            return new CustomQueryable<TEntity>
+            public virtual async Task<ICustomQueryable<TEntity>> GetAllWithPagingAsync(
+                IPaging paging, Expression<Func<TEntity, bool>> predicate = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, bool selectPassives = false
+            )
             {
-                Result = query,
-                Count = rowCount
-            };
-        }
+                IQueryable<TEntity> query = DbSet;
 
-        #endregion
+                query = query.Where(predicate != null ? PredicateBuild(predicate, selectPassives) : PredicateBuild(selectPassives));
 
-        #region IDisposable members
-        ~AbstractRepository()
-        {
-            Dispose(false);
-        }
+                var rowCount = await query.CountAsync();
+                query = _includeProperties.Aggregate(query, (current, includedProperty) => current.Include(includedProperty));
 
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
+                if (orderBy != null)
+                {
+                    query = orderBy(query);
+                }
 
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposing && !_disposed)
-            {
-                DbContext.Dispose();
+                query = query.Skip(Math.Abs(paging.PageIndex - 1) * paging.PageSize).Take(paging.PageSize);
+
+                return new CustomQueryable<TEntity>
+                {
+                    Result = query,
+                    Count = rowCount
+                };
             }
 
-            _disposed = true;
-        }
+            public virtual ICustomQueryable<TEntity> GetAllWithPaging(
+                IPaging paging, Expression<Func<TEntity, bool>> predicate = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, bool selectPassives = false
+            )
+            {
+                IQueryable<TEntity> query = DbSet;
 
-        private static Expression<Func<TEntity, bool>> PredicateBuild(Expression<Func<TEntity, bool>> predicate)
-        {
-            return predicate.And(p => (int)p.Status == (int)Status.Active);
-        }
+                query = query.Where(predicate != null ? PredicateBuild(predicate, selectPassives) : PredicateBuild(selectPassives));
 
-        private static Expression<Func<TEntity, bool>> PredicateBuild()
-        {
-            var predicate = PredicateBuilder.New<TEntity>();
-            return predicate.And(p => (int)p.Status == (int)Status.Active);
-        }
+                var rowCount = query.Count();
+                query = _includeProperties.Aggregate(query, (current, includedProperty) => current.Include(includedProperty));
 
-        #endregion
-    }
+                if (orderBy != null)
+                {
+                    query = orderBy(query);
+                }
+
+                query = query.Skip((paging.PageIndex - 1) * paging.PageSize).Take(paging.PageSize);
+
+                return new CustomQueryable<TEntity>
+                {
+                    Result = query,
+                    Count = rowCount
+                };
+            }
+
+            #endregion
+
+            #region IDisposable members
+            ~AbstractRepository()
+            {
+                Dispose(false);
+            }
+
+            public void Dispose()
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+
+            protected virtual void Dispose(bool disposing)
+            {
+                if (disposing && !_disposed)
+                {
+                    DbContext.Dispose();
+                }
+
+                _disposed = true;
+            }
+
+            private static Expression<Func<TEntity, bool>> PredicateBuild(Expression<Func<TEntity, bool>> predicate, bool selectPassives = false)
+            {
+                if (selectPassives) return predicate.And(p => (int) p.Status == (int) Status.Active || (int) p.Status == (int) Status.Passive);
+                return predicate.And(p => (int) p.Status == (int) Status.Active);
+            }
+
+            private static Expression<Func<TEntity, bool>> PredicateBuild(bool selectPassives = false)
+            {
+                var predicate = PredicateBuilder.New<TEntity>();
+
+                if (selectPassives) return predicate.And(p => (int) p.Status == (int) Status.Active || (int) p.Status == (int) Status.Passive);
+                return predicate.And(p => (int) p.Status == (int) Status.Active);
+            }
+
+            #endregion
+        }
 }
--- CustomFramework.Data/Repositories/IBaseRepository.cs
+++ CustomFramework.Data/Repositories/IBaseRepository.cs
@@ -8,30 +8,24 @@ using CustomFramework.Data.Models;
 namespace CustomFramework.Data.Repositories
 {
     public interface IBaseRepository<TEntity, in TKey> : IDisposable
-        where TEntity : BaseModelNonUser<TKey>
+    where TEntity : BaseModelNonUser<TKey>
     {
         Task<TEntity> GetByIdAsync(TKey id);
 
         TEntity GetById(TKey id);
 
-        IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> predicate);
+        IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> predicate, bool selectPassives = false);
 
         IQueryable<TEntity> GetAll(
-            Expression<Func<TEntity, bool>> predicate = null
-            , Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null
-            , int? take = null
+            Expression<Func<TEntity, bool>> predicate = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, int? take = null, bool selectPassives = false
         );
 
         Task<ICustomQueryable<TEntity>> GetAllWithPagingAsync(
-            IPaging paging
-            , Expression<Func<TEntity, bool>> predicate = null
-            , Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null
+            IPaging paging, Expression<Func<TEntity, bool>> predicate = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, bool selectPassives = false
         );
 
         ICustomQueryable<TEntity> GetAllWithPaging(
-            IPaging paging
-            , Expression<Func<TEntity, bool>> predicate = null
-            , Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null
+            IPaging paging, Expression<Func<TEntity, bool>> predicate = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, bool selectPassives = false
         );
     }
 }
