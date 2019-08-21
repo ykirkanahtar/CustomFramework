d635ef911807942f5c28e4d7d0646af25aee69d0
Yunus Emre <yunusemre.kirkanahtar@cutesafe.com>
2019-08-21 16:58:42 +0300

    Çeşitli değişiklikler yapıldı

--- CustomFramework.WebApiUtils.Identity/Models/CustomUser.cs
+++ CustomFramework.WebApiUtils.Identity/Models/CustomUser.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Identity;
 namespace CustomFramework.WebApiUtils.Identity.Models
 {
     public class CustomUser : IdentityUser<int>
-    {
+    {        
         public string FirstName { get; set; }
         public string Surname { get; set; }
         public DateTime BirthDate { get; set; }
