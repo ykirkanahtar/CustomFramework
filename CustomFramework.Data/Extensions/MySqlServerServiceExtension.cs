f4dffce38f52510d2ef443328725aa4c09a7c923
Yunus Emre <yunusemre.kirkanahtar@cutesafe.com>
2019-09-12 15:31:26 +0300

    MySql desteği eklendi

---
+++ CustomFramework.Data/Extensions/MySqlServerServiceExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CustomFramework.Data.Extensions
+{
+    public static class MySqlServerServiceExtension
+    {
+        public static IServiceCollection AddMySqlServer<TContext>(this IServiceCollection services, string connectionString, bool lazyLoading = false) where TContext : DbContext
+        {
+            services.AddDbContext<TContext>(options =>
+                {
+                    options.UseMySql(connectionString);
+                    options.UseLazyLoadingProxies(lazyLoading);
+                }
+            );
+
+            services.AddDbContext<TContext>();
+
+            return services;
+        }
+    }
+}
