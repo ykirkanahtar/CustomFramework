44b95fa23a7893d4a2e5c76ed79517580240c5f0
Yunus Emre <yunusemre.kirkanahtar@cutesafe.com>
2019-09-16 09:56:25 +0300

    E-posta doğrulaması yapan metot yazıldı

---
+++ CustomFramework.Utils/Functions.cs
@@ -0,0 +1,20 @@
+using System.Net.Mail;
+
+namespace CustomFramework.Utils
+{
+    public static class Functions
+    {
+        public static bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
